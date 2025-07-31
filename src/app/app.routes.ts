138cb61caa7d08cf68cccb68aedd02af9e208715
sotol067 <luz.soto.-nd@disney.com>
2025-07-31 06:13:15 -0500

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ShoppingCartComponent } from './views/shopping-cart/shopping-cart.component';
+import { StoreViewComponent } from './views/store-view/store-view.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+const appBaseTitle: string = 'Restaurant UI - '
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: appBaseTitle + 'Home'
+  },
+  {
+    path: 'Home',
+    component: HomeComponent,
+    title: appBaseTitle + 'Home'
+  },
+  {
+    path: 'Shopping cart',
+    component: ShoppingCartComponent,
+    title: appBaseTitle + 'Shopping cart'
+  },
+  {
+    path: 'Store',
+    component: StoreViewComponent,
+    title: appBaseTitle + 'Store'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
