261ba4ad796925f9898cad7ba2f47c1c9f0a244e
sotol067 <luz.soto.-nd@disney.com>
2025-08-07 07:04:13 -0500

    Theme type added and created theme and shape attributes on button component ts. buttonText input parameter included. Theme conditions added on home component html. disable attribute added. Created ButtonTheme y ButtonShape interfaces

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { ButtonShape, ButtonTheme } from '../../model/interfaces';
 @Component({
   selector: 'rui-button',
   standalone: true,
@@ -8,7 +8,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-  @Input() buttonText: string ='';
+  @Input({ required: true}) buttonText: string ='';
   @Input({ required: true}) ariaLabel: string = '';
+  @Input() theme: ButtonTheme = 'primary'
+  @Input() shape: ButtonShape = 'square'
   @Output() onClick = new EventEmitter<string>();
 }
